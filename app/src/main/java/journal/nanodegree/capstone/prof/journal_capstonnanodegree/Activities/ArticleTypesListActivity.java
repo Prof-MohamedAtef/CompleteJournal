699a69f5e17bf674b09e1eacb1b857befa036359
Prof-MohamedAtef <proofesser@gmail.com>
2019-01-25 11:50:08 -0800

    Added ButterKnife, BackButton for TypesList, AddArticle Activities

--- app/src/main/java/journal/nanodegree/capstone/prof/journal_capstonnanodegree/Activities/ArticleTypesListActivity.java
+++ app/src/main/java/journal/nanodegree/capstone/prof/journal_capstonnanodegree/Activities/ArticleTypesListActivity.java
@@ -1,31 +1,21 @@
 package journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Optional;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.BuildConfig;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.Fragments.ArticlesMasterListFragment;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.Fragments.FragmentArticleViewer;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.Fragments.FragmentSoundPlayer;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.R;
 import journal.nanodegree.capstone.prof.journal_capstonnanodegree.helpers.OptionsEntity;
-
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.ARTS;
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.ArticleType;
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.BUSINESS;
@@ -37,7 +27,6 @@ import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activit
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.REPORTS;
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.SPORTS;
 import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.Activities.HomeActivity.TECHNOLOGY;
-import static journal.nanodegree.capstone.prof.journal_capstonnanodegree.BuildConfig.token;
 
 public class ArticleTypesListActivity extends AppCompatActivity implements ArticlesMasterListFragment.OnSelectedArticleListener,
         SwipeRefreshLayout.OnRefreshListener{
@@ -55,24 +44,23 @@ public class ArticleTypesListActivity extends AppCompatActivity implements Artic
     public static String WebHoseAPIKEY="WenHoseAPIKEY";
     private String Arts;
     private String ArticleType_;
-//    @BindView(R.id.toolbar)
+    @BindView(R.id.toolbar)
     Toolbar mToolbar;
-//    @BindView(R.id.swipe_refresh_layout)
+    @BindView(R.id.swipe_refresh_layout)
     SwipeRefreshLayout mSwipeRefreshLayout;
-//    @BindView(R.id.coordinator_layout)
-//    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_types_list);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
         if (findViewById(R.id.ArticleDetails)!=null) {
             mTwoPaneUi = true;
         }else {
             mTwoPaneUi = false;
-            mToolbar=(Toolbar)findViewById(R.id.toolbar);
             setSupportActionBar(mToolbar);
             assert getSupportActionBar()!=null;
             getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -83,7 +71,6 @@ public class ArticleTypesListActivity extends AppCompatActivity implements Artic
                     onBackPressed();
                 }
             });
-            mSwipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swipe_refresh_layout);
             mSwipeRefreshLayout.setOnRefreshListener(this);
         }
         token= BuildConfig.token;
